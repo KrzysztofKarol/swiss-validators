72bda5cd8119bd593510b6671066eb845beaa920
Krzysztof Karol <krzysztof.karol@gmail.com>
2025-01-18 23:06:48 +0100

    Use vitest

--- packages/swissid/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  moduleFileExtensions: ["ts", "tsx", "js"],
-  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
-  testPathIgnorePatterns: [
-    "/node_modules/",
-    "(/__tests__/.*|(\\.|/)(test|spec))\\.d.ts$",
-  ],
-};
--- packages/swissid/src/__tests__/swissid.test.ts
+++ packages/swissid/src/__tests__/swissid.test.ts
@@ -1,3 +1,4 @@
+import { describe, it, expect } from "vitest";
 import validator from "swiss-validator";
 
 describe("swissid", () => {
